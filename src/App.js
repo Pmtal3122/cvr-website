d7405be3309731b715f670db586d83e5d103d903
Pmtal3122 <talegaonkar.piyush@gmail.com>
2024-02-23 13:23:41 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+import '../src/constants/products.json'
+import { prods, prodImages } from './constants/productsArray';
+import ProductsDisplay from './Components/ProductsDisplay/ProductsDisplay';
+// import { prodImages } from './constants/productsImages';
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    // console.log(prods)
+    console.log(data);
+    console.log(typeof (data))
+    console.log(Object.values(data));
+  }, [data])
+
+  let voiceRecog = () => {
+    axios.get("http://127.0.0.1:5000/")
+      .then(res => {
+        setData(res.data);
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='bodyDiv'>
+      <ProductsDisplay />
+      <div id="recogButton">
+        <button onClick={voiceRecog}>RECOMMEND PRODUCTS</button>
+      </div>
+      <div id="displayRecommended">
+        {Object.values(data).map(ele => (
+          <li>
+            {prods[ele].toUpperCase()}
+            <img width="200px" src={prodImages[ele]} alt="" />
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/constants/productsImages.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import img0 from '../images/0_Backpack.jpg'
+import img1 from '../images/1_Sneakers.jpg'
+import img2 from '../images/2_Dress.jpeg'
+import img3 from '../images/3_Sunglasses.jpg'
+import img4 from '../images/4_Hat.jpg'
+import img5 from '../images/5_Gloves.jpg'
+import img6 from '../images/6_Tshirt.jpg.webp'
+import img7 from '../images/7_Jeans.jpeg'
+import img8 from '../images/8_Watch.jpg'
+import img9 from '../images/9_Belt.jpg'
+
+class productsImages extends Component {
+    constructor(props) {
+        super(props);
+        this.prodImages = [img0, img1, img2, img3, img4, img5, img6, img7, img8, img9];
+    }
+}
+
+export const prodImages = new productsImages().prodImages;
