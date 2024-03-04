5b5ad0e121ee64c8af960d46c84b4908299272c6
Pmtal3122 <talegaonkar.piyush@gmail.com>
2024-03-04 10:33:57 +0530

    Client side data being sent successfully

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import './App.css';
 import axios from 'axios'
 import { useEffect, useState } from 'react';
 import '../src/constants/products.json'
+import recDataJson from './constants/recommenderData.json'
 import { prods, prodImages } from './constants/productsArray';
 import ProductsDisplay from './Components/ProductsDisplay/ProductsDisplay';
 // import { prodImages } from './constants/productsImages';
 
 function App() {
   const [data, setData] = useState("");
+  
 
   useEffect(() => {
     // console.log(prods)
     console.log(data);
     console.log(typeof (data))
     console.log(Object.values(data));
+    if(!localStorage.getItem("recData")) {
+      const recData = JSON.stringify(recDataJson);
+      console.log(recData);
+      localStorage.setItem("recData", recData);
+    }
   }, [data])
 
   let voiceRecog = () => {
-    axios.get("http://127.0.0.1:5000/")
+    axios.get("http://127.0.0.1:5000/", {
+      params: {
+        recData: JSON.stringify(recDataJson)
+      }
+    })
       .then(res => {
         setData(res.data);
       })
