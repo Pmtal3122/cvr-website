e9e7859f457bb9fa444c28b94793fa6a81d5794b
Pmtal3122 <talegaonkar.piyush@gmail.com>
2024-04-10 10:35:24 +0530

    Added extra five categories

--- src/constants/productsArray.js
+++ src/constants/productsArray.js
@@ -9,12 +9,17 @@ import img6 from '../images/6_Tshirt.jpg.webp'
 import img7 from '../images/7_Jeans.jpeg'
 import img8 from '../images/8_Watch.jpg'
 import img9 from '../images/9_Belt.jpg'
+import img10 from '../images/10_Laptop.jpg'
+import img11 from '../images/11_Mobile.webp'
+import img12 from '../images/12_Book.jpg'
+import img13 from '../images/13_Sports.jpg'
+import img14 from '../images/14_Gaming.jpg'
 
 class productsArray extends Component{
     constructor(props){
         super(props);
-        this.prods = ["backpacks", "sneakers", "dress", "sunglasses", "hat", "gloves", "t-shirts", "jeans", "watch", "belt"]
-        this.prodImages = [img0, img1, img2, img3, img4, img5, img6, img7, img8, img9];
+        this.prods = ["backpacks", "sneakers", "dress", "sunglasses", "hat", "gloves", "t-shirts", "jeans", "watch", "belt", "laptop", "mobile", "book", "sports", "gaming"];
+        this.prodImages = [img0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14];
     }
 }
 
